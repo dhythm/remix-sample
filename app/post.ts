fc91a384eeabce359456358e3fd88f1ef164ccca
dhythm <yuta.okada.20@gmail.com>
2021-12-05 18:34:55 +0900

    work until Pulling from a data source

--- app/post.ts
+++ app/post.ts
@@ -1,18 +1,39 @@
+import path from "path";
+import fs from "fs/promises";
+import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
+
 export type Post = {
   slug: string;
   title: string;
 };
 
-export function getPosts() {
-  const posts: Post[] = [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
-  return posts;
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
+const postsPath = path.join(__dirname, "..", "posts");
+
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
+
+export async function getPosts() {
+  const dir = await fs.readdir(postsPath);
+  return Promise.all(
+    dir.map(async (filename) => {
+      const file = await fs.readFile(path.join(postsPath, filename));
+      const { attributes } = parseFrontMatter(file.toString());
+      invariant(
+        isValidPostAttributes(attributes),
+        `${filename} has bad meta data!`
+      );
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title,
+      };
+    })
+  );
 }
